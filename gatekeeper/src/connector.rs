2c04405e69beb61ec1cdeec7bf65c61359ea3ff4
takayuki goto <takayuki@idein.jp>
2020-03-24 17:24:33 +0900

    Fix: resolving domain name

--- gatekeeper/src/connector.rs
+++ gatekeeper/src/connector.rs
@@ -1,12 +1,9 @@
-use std::net::{TcpStream, ToSocketAddrs};
-
-use failure::ResultExt;
-use log::*;
+use std::net::TcpStream;
 
 use crate::byte_stream::ByteStream;
 use crate::pkt_stream::{PktStream, UdpPktStream};
 
-use model::error::{Error, ErrorKind};
+use model::error::Error;
 use model::model::*;
 
 pub trait Connector: Send {
@@ -19,28 +16,15 @@ pub trait Connector: Send {
 #[derive(Debug, Clone)]
 pub struct TcpUdpConnector;
 
-impl TcpUdpConnector {
-    fn resolve(&self, addr: Address) -> Result<SocketAddr, Error> {
-        let sock_addr = match addr {
-            Address::IpAddr(addr, port) => SocketAddr::new(addr, port),
-            Address::Domain(host, port) => {
-                let mut iter = (host.as_str(), port)
-                    .to_socket_addrs()
-                    .context(ErrorKind::DomainNotResolved { domain: host, port }.into())?;
-                iter.next().unwrap()
-            }
-        };
-        trace!("resolved: {}", sock_addr);
-        Ok(sock_addr)
-    }
-}
-
 impl Connector for TcpUdpConnector {
     type B = TcpStream;
     type P = UdpPktStream;
     fn connect_byte_stream(&self, addr: Address) -> Result<Self::B, Error> {
-        let sock_addr = self.resolve(addr)?;
-        TcpStream::connect(sock_addr).map_err(Into::into)
+        match addr {
+            Address::IpAddr(addr, port) => TcpStream::connect(SocketAddr::new(addr, port)),
+            Address::Domain(host, port) => TcpStream::connect((host.as_str(), port)),
+        }
+        .map_err(Into::into)
     }
     fn connect_pkt_stream(&self, _addr: Address) -> Result<Self::P, Error> {
         unimplemented!("connect_pkt_stream")
