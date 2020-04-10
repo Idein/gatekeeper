3f3b8d2be891f09d3946e1700d667391eaff44f6
takayuki goto <takayuki@idein.jp>
2020-04-10 16:30:13 +0900

    Fix: integration test

--- src/test.rs
+++ src/test.rs
@@ -4,9 +4,11 @@ use std::path::PathBuf;
 #[test]
 #[ignore]
 fn get_main() {
-    use log::*;
-    use socks::*;
     use std::io::prelude::*;
+
+    use log::*;
+    use regex::Regex;
+    use socks::*;
 
     let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     println!("root: {}", root.display());
@@ -22,21 +24,27 @@ fn get_main() {
         .unwrap();
 
         // request main.rs
-        write!(conn, "GET /gatekeeper/src/main.rs HTTP/1.1\r\n").unwrap();
+        write!(conn, "GET /src/main.rs HTTP/1.1\r\n").unwrap();
         write!(conn, "Host: myhttpd\r\n\r\n").unwrap();
         conn.flush().unwrap();
 
         let mut conn = std::io::BufReader::new(conn);
         // skip http headers
         let mut line = String::new();
+        let mut content_length = None;
+        let re = Regex::new(r"Content-Length: (\d+)\r\n").unwrap();
         while let Ok(_) = conn.read_line(&mut line) {
             debug!("line: {:?}", line);
             if line == "\r\n" {
                 break;
             }
+            if let Some(m) = re.captures(&line) {
+                content_length = m.get(1).unwrap().as_str().parse().ok();
+            }
             line.clear();
         }
-        let mut buff = vec![0; exp.len()];
+        let mut buff = Vec::new();
+        buff.resize(content_length.unwrap(), 0);
         conn.read_exact(&mut buff[..]).unwrap();
         String::from_utf8_lossy(&buff).to_string()
     };
